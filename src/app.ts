cdd552b14a33467dcf98c3376fc8a3a7bababcf1
Johannes Gerold <git+commit@jgero.me>
2023-06-09 19:06:42 +0200

    refactor(matrix-client): split matrix client logic from app

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import * as sdk from "matrix-js-sdk";
 import { Command } from "./commands/command";
 import { StartCommand } from "./commands/start-command";
 import { Config } from "./config";
@@ -6,23 +5,24 @@ import { DeliveredCommand } from "./commands/delivered-command";
 import { OrderCommand } from "./commands/order-command";
 import { HelpCommand } from "./commands/help-command";
 import { StateMachine } from "./state-machine";
+import { MatrixClientFacade } from "./matrix-client-facade";
 
 export class App {
   private config: Config = new Config();
 
   private commandList: Command[] = [];
   private stateMachine: StateMachine;
+  private matrixClient: MatrixClientFacade;
 
-  private matrixClient: sdk.MatrixClient = sdk.createClient({
-    baseUrl: this.config.getBaseUrl(),
-    accessToken: this.config.getAccessToken(),
-    userId: this.config.getUserId(),
-  });
+  public constructor(
+    stateMachine: StateMachine,
+    matrixClient: MatrixClientFacade
+  ) {
+    this.stateMachine = stateMachine;
+    this.matrixClient = matrixClient;
+    this.matrixClient.sendMessage(".inder is back!");
 
-  public constructor() {
-    this.stateMachine = new StateMachine();
-    this.matrixClient.startClient();
-    this.sendMessage(".inder is back!");
+    this.matrixClient.listenToRoomEvents(this.processMessage);
 
     console.log(this.config.getRoomId());
 
@@ -31,30 +31,6 @@ export class App {
     this.commandList.push(new DeliveredCommand(this));
     this.commandList.push(new OrderCommand(this));
     this.commandList.push(new HelpCommand(this));
-
-    /* listen to matrix messages */
-    this.matrixClient.on(
-      sdk.RoomEvent.Timeline,
-      (event, room, toStartOfTimeline) => {
-        // only listen to messages in the given room which were not sent by this bot
-        if (
-          event.getType() !== "m.room.message" ||
-          event.event.room_id != this.config.getRoomId() ||
-          event.event.sender == this.config.getUserId()
-        ) {
-          return;
-        }
-
-        if (
-          event?.event?.content?.body &&
-          event.event.origin_server_ts &&
-          Math.floor(Date.now()) - event.event.origin_server_ts <= 1000
-        ) {
-          //sendMessage("[" + event.event.sender + "] " + event.event.content.body);
-          this.processMessage(event.event.content.body);
-        }
-      }
-    );
   }
 
   private processMessage(message: string): void {
@@ -70,20 +46,9 @@ export class App {
 
   /* send a matrix message */
   public sendMessage(message: string): void {
-    const app = this; //App.getInstance();
-
-    const content: any = {
-      body: message,
-      msgtype: "m.text",
-    };
-    app.matrixClient.sendEvent(
-      app.config.getRoomId(),
-      "m.room.message",
-      content,
-      ""
-    );
+    this.matrixClient.sendMessage(message);
   }
 }
 
 require("dotenv").config();
-new App();
+new App(new StateMachine(), new MatrixClientFacade(new Config()));
