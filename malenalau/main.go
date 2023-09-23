8ac5bb7e1b0b287e2342bc1fbde8d96e3c2892b3
Johannes Gerold <git+commit@jgero.me>
2023-09-24 00:00:23 +0200

    feat(malenalau): take options via CLI parameters

--- malenalau/main.go
+++ malenalau/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
+
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -17,23 +21,54 @@ const (
 )
 
 func main() {
+	var homeServerUrl, user, passwordFile, omegaStarUrl, galactusUrl string
+	var rooms RoomsFlag
+	flag.StringVar(&homeServerUrl, "home-server-url", "matrix.org", "matrix home server url, e.g. matrix.org")
+	flag.StringVar(&user, "user", "", "username of the matrix account")
+	flag.StringVar(&passwordFile, "password-file", "", "location of the file with the passowrd for the matrix account")
+	flag.StringVar(&omegaStarUrl, "omega-star", "http://localhost:8081", "URL where the omega star service can be reached")
+	flag.StringVar(&galactusUrl, "galactus", "http://localhost:8080", "URL where the galactus service can be reached")
+	flag.Var(&rooms, "room", "repeatable flag with matrix room ids the bot should join")
+	flag.Parse()
+
+	log.Debug().Msgf("user: '%s'", user)
+
+	b, err := os.ReadFile(passwordFile)
+	if err != nil {
+		log.Fatal().Err(err).Msgf("could not read password file '%s'", passwordFile)
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
-	ctx = context.WithValue(ctx, HomeServerURLKey, "matrix.org")
-	ctx = context.WithValue(ctx, PasswordKey, "2sm^ziai#95nRHoMVzKz")
-	ctx = context.WithValue(ctx, UserKey, "order-bot-aa")
-	ctx = context.WithValue(ctx, OmegaStarURLKey, "http://localhost:8081")
-	ctx = context.WithValue(ctx, GalactusURLKey, "http://localhost:8080")
+	ctx = context.WithValue(ctx, HomeServerURLKey, homeServerUrl)
+	ctx = context.WithValue(ctx, PasswordKey, strings.TrimSpace(string(b)))
+	ctx = context.WithValue(ctx, UserKey, user)
+	ctx = context.WithValue(ctx, OmegaStarURLKey, omegaStarUrl)
+	ctx = context.WithValue(ctx, GalactusURLKey, galactusUrl)
 
 	facade := NewGalactusFacade(ctx, time.Second*10)
 	parser := NewMessageParser(ctx, ".")
 	runner := NewActionRunner(facade)
 	bot := NewMatrixBot(ctx, parser, runner)
 
-	bot.LoginAndJoin([]string{"!hvJGXMkjcyzxtSNNsx:matrix.org"})
+	bot.LoginAndJoin(rooms)
 	go bot.Listen()
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
 	cancel()
+}
+
+type RoomsFlag []string
+
+func (rooms *RoomsFlag) Set(value string) error {
+	*rooms = append(*rooms, value)
+	return nil
+}
+
+func (rooms *RoomsFlag) String() (out string) {
+	out = "["
+	out += strings.Join(*rooms, ",")
+	out += "]"
+	return
 }
