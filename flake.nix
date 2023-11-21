47aec06ca57ad6134005532c07d2e8d4b81612f5
Johannes Gerold <git+commit@jgero.me>
2023-11-21 21:25:22 +0100

    fix: formatting

--- flake.nix
+++ flake.nix
@@ -29,23 +29,23 @@
       packages.${system} = {
         omega-star-bin = omega-star.bin;
         omega-star-dev = pkgs.writeScriptBin "omega-star-dev" ''
-        ADDRESS=127.0.0.1:8080 ${omega-star.bin}/bin/omega-star -v
+          ADDRESS=127.0.0.1:8080 ${omega-star.bin}/bin/omega-star -v
         '';
         omega-star = omega-star.container;
         galactus-bin = galactus.bin;
         galactus-dev = pkgs.writeScriptBin "galactus-dev" ''
-        ADDRESS=127.0.0.1:8081 OMEGA_STAR_URL=http://localhost:8080 ${galactus.bin}/bin/galactus -v
+          ADDRESS=127.0.0.1:8081 OMEGA_STAR_URL=http://localhost:8080 ${galactus.bin}/bin/galactus -v
         '';
         galactus = galactus.container;
         malenalau-bin = malenalau.bin;
         malenalau-dev = pkgs.writeScriptBin "malenalau-dev" ''
-        export ROOM="!hvJGXMkjcyzxtSNNsx:matrix.org"
-        export USER=order-bot-aa
-        export HOME_SERVER=matrix.org
-        export PASSWORD_FILE="./secret.txt"
-        export OMEGA_STAR_URL=http://localhost:8080
-        export GALACTUS_URL=http://localhost:8081
-        ${malenalau.bin}/bin/malenalau -v
+          export ROOM="!hvJGXMkjcyzxtSNNsx:matrix.org"
+          export USER=order-bot-aa
+          export HOME_SERVER=matrix.org
+          export PASSWORD_FILE="./secret.txt"
+          export OMEGA_STAR_URL=http://localhost:8080
+          export GALACTUS_URL=http://localhost:8081
+          ${malenalau.bin}/bin/malenalau -v
         '';
         malenalau = malenalau.container;
       };
