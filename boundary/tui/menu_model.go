6296b7e2f2a6ed3e15f82b53e925ba39fcb45200
Johannes Gerold <git+commit@jgero.me>
2024-05-31 01:45:38 +0200

    feat: add searching for menu

--- boundary/tui/menu_model.go
+++ boundary/tui/menu_model.go
@@ -2,7 +2,9 @@ package tui
 
 import (
 	"fmt"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
@@ -16,15 +18,20 @@ type MenuModel struct {
 	*LayoutInfo
 	s        lipgloss.Style
 	viewport viewport.Model
+	it       textinput.Model
 	menu     *entity.Menu
 	ready    bool
 }
 
 func NewMenuModel(info *LayoutInfo) *MenuModel {
 	vp := viewport.New(info.width, info.height-info.headerOffset-info.footerOffset)
+	it := textinput.New()
+	it.Placeholder = "Search"
+	it.Width = 20
 	return &MenuModel{
 		LayoutInfo: info,
 		s:          info.txtStyle,
+		it:         it,
 		viewport:   vp,
 		ready:      false,
 	}
@@ -51,7 +58,8 @@ func (m *MenuModel) Init() tea.Cmd {
 
 func (m *MenuModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-	m.viewport, cmd = m.viewport.Update(msg)
+	// m.viewport, cmd = m.viewport.Update(msg)
+	m.it, cmd = m.it.Update(msg)
 	return m, cmd
 }
 
@@ -61,16 +69,20 @@ func (m *MenuModel) View() string {
 	}
 	rows := make([][]string, 0)
 	for _, item := range m.menu.Items {
-		rows = append(rows, []string{item.ShortName, item.Name, fmt.Sprintf("%4.2f€", float64(item.Price)/100.0)})
+		if strings.Contains(strings.ToLower(item.Name), strings.ToLower(m.it.Value())) ||
+			strings.Contains(strings.ToLower(item.ShortName), strings.ToLower(m.it.Value())) {
+			rows = append(rows, []string{item.ShortName, item.Name, fmt.Sprintf("%4.2f€", float64(item.Price)/100.0)})
+		}
 	}
 	table := table.New().
 		BorderStyle(m.txtStyle).
 		Width(m.width).
 		Headers("SHORT", "NAME", "PRICE").
 		Rows(rows...)
-	c := m.s.Render(table.Render())
-	m.viewport.SetContent(c)
-	m.viewport.Height = m.height - m.headerOffset - m.footerOffset
+	search := m.it.View() + "\n\n"
+	m.viewport.SetContent(m.s.Render(table.Render()))
+	m.viewport.Height = m.height - m.headerOffset - m.footerOffset - lipgloss.Height(search)
 	m.viewport.YPosition = m.headerOffset
-	return m.viewport.View()
+	m.it.Focus()
+	return search + m.viewport.View()
 }
